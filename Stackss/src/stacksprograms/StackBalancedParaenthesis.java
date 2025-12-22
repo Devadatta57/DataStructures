61b9088b49d01b5fe965c1fa83565a93052ef810
Devadatta57 <193812273+Devadatta57@users.noreply.github.com>
2025-12-22 09:57:32 +0530

    Update StackBalancedParaenthesis.java

--- Stackss/src/stacksprograms/StackBalancedParaenthesis.java
+++ Stackss/src/stacksprograms/StackBalancedParaenthesis.java
@@ -3,44 +3,38 @@ package stacksprograms;
 import java.util.Stack;
 
 
-class Paraenthesis{
-	static boolean BalPara(String ss) {
-		Stack<Character> s= new Stack<Character>();
-		
-		char[] ch=ss.toCharArray();
-		for(char c:ch) {
-		     switch(c) {
-		     case '(':
-		    	      s.push(c);
-		              break;
-		     case '{':
-		    	      s.push(c);
-		    	      break;
-		     case '[':
-	    	         s.push(c);
-	    	         break;	
-		     case ')':
-	    	         if(s.pop()!='(') {	    	          	
-	    	           return false;	  
-	    	         }
-	    	         break;
-	    	          
-	         case '}':
-	    	          if(s.pop()!='{') {
-	    	          return false;
-	    	          }
-	    	          break;
-	        case ']':
-	        	    if(s.pop()!='[')   {
-  	                   return false;
-	        	    }
-	        	    break;
-		     }
-		}
-		 
-		return s.isEmpty();
-	
-	}
+static boolean BalPara(String ss) {
+    Stack<Character> s = new Stack<>();
+
+    for (char c : ss.toCharArray()) {
+        switch (c) {
+            case '(':
+            case '{':
+            case '[':
+                s.push(c);
+                break;
+
+            case ')':
+                if (s.isEmpty() || s.pop() != '(')
+                    return false;
+                break;//when correct case condition break and go to nxt char
+
+            case '}':
+                if (s.isEmpty() || s.pop() != '{')
+                    return false;
+                break;
+
+            case ']':
+                if (s.isEmpty() || s.pop() != '[')
+                    return false;
+                break;
+        }
+    }
+
+    // FINAL check
+    return s.isEmpty();
+}
+
 }
 public class StackBalancedParaenthesis {
 	public static void main(String[] args) {
