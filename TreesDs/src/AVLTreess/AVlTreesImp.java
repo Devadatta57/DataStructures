db00820f1d5102ad70d2d9e08ac4bc12361e4e08
Devadatta57 <193812273+Devadatta57@users.noreply.github.com>
2026-01-08 23:16:29 +0530

    Update AVlTreesImp.java

--- TreesDs/src/AVLTreess/AVlTreesImp.java
+++ TreesDs/src/AVLTreess/AVlTreesImp.java
@@ -108,3 +108,119 @@ public class AVlTreesImp {
       a.InOrderTraversal(a.root);
 	}
 }
+
+
+
+//class Node{
+// 	int data;
+// 	Node left;
+// 	Node right;
+// 	int ht;
+	
+// 	Node(int data){
+// 		this.data=data;
+// 		this.left=null;
+// 		this.right=null;
+// 		this.ht=1;
+// 	}
+// }
+// class avl{
+// 	Node node;
+	
+// 	avl(){
+// 		node=null;
+// 	}
+// 	int getBalance(Node node) {
+// 		return height(node.left)-height(node.right);
+// 	  }
+
+// 	int height(Node node) {
+// 		if(node==null) {
+// 			return 0;
+// 		}
+// 		return node.ht;
+// 	}
+	
+// 	public Node insert(Node node,int value) {
+// 		if(node==null) {
+// 			return new Node(value);
+// 		}
+// 		else if(value<node.data) {
+// 			node.left=insert(node.left, value);
+// 		}
+// 		else if(value>node.data) {
+// 			node.right=insert(node.right, value);
+// 		}
+// 		else {
+// 			return node;
+// 		}
+		
+// 		node.ht=1+Math.max(height(node.left),height(node.right));
+		
+// 		int bf=getBalance(node);
+		
+// 		if(bf<-1 && value>node.right.data) {
+// 			return leftRotation(node);
+// 		}
+// 		 if(bf<-1 && value<node.right.data) {
+// 			node.right=RightRotation(node.right);
+// 			return leftRotation(node);
+// 		}
+// 		 if(bf>1 && value<node.left.data) {
+// 			return RightRotation(node);
+// 		 }
+// 		 if(bf>1 && value>node.left.data) {
+// 			    node.left=leftRotation(node.left);
+// 				return RightRotation(node);
+// 			 }
+// 		return node;
+// 	}
+
+// 	   Node  RightRotation(Node y) {
+// 		  Node x=y.left;
+// 		  Node T=x.right;
+		  
+// 		  x.right=y;
+// 		  y.left=T;
+		  
+// 		  y.ht=1+Math.max(height(y.left), height(y.right));
+// 		  x.ht=1+Math.max(height(x.left), height(x.right));
+// 			return x;
+// 	   }
+
+// 	  Node leftRotation(Node y) {
+// 		Node x=y.right;
+// 		Node T=x.left;
+		
+// 		x.left=y;
+// 		y.right=T;
+		
+// 		y.ht=1+Math.max(height(y.left), height(y.right));
+// 		x.ht=1+Math.max(height(x.left), height(x.right));
+// 		return x;
+// 	  }
+
+// 	  public void InOrderTraversal(Node node) {
+// 		  if(node==null) {
+// 			  return;
+// 		  }
+// 		  InOrderTraversal(node.left);
+// 		  System.out.print(node.data+" ");
+// 		  InOrderTraversal(node.right);
+// 	  }
+	
+// }
+// public class AVLTree {
+// 	public static void main(String[] args) {
+// 		avl a= new avl();
+// 		a.node=a.insert(a.node, 10);
+// 		a.node=a.insert(a.node, 33);
+// 		a.node=a.insert(a.node, 78);
+// 		a.node=a.insert(a.node, 8);
+// 		a.node=a.insert(a.node, 18);
+// 		a.InOrderTraversal(a.node);
+		
+		
+// 	}
+// }
+
