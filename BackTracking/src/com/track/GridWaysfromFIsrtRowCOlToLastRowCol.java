d981413d57cceda9dd5b650a6ad6dfa6b06b27bb
Devadatta57 <193812273+Devadatta57@users.noreply.github.com>
2026-01-11 17:43:11 +0530

    Update GridWaysfromFIsrtRowCOlToLastRowCol.java

--- BackTracking/src/com/track/GridWaysfromFIsrtRowCOlToLastRowCol.java
+++ BackTracking/src/com/track/GridWaysfromFIsrtRowCOlToLastRowCol.java
@@ -2,13 +2,13 @@ package com.track;
 
 class Grid{
 	public static int GridWays(int i ,int j,int n ,int m) {
-		if(i==n-1 && j==m-1)
+		if(i==n-1 && j==m-1)//if row and col reaches destination return 1 so one way
 			return 1;
 		else if(i==n || j==m)
-			return 0;
-		int value1=GridWays(i+1, j, n, m);
-		int value2=GridWays(i, j+1, n, m);
-		return value1+value2;
+			return 0;//if row or col goes out of grid even anyone return 0 path
+		int value1=GridWays(i+1, j, n, m);//go down
+		int value2=GridWays(i, j+1, n, m);//go right
+		return value1+value2;//how many ways total from up and down 
 		
 	}
 }
